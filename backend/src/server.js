fd635d71360a93ae5099c2f48197013e0b9dd008
Hebert Lima <hebert.dev@hotmail.com>
2019-10-03 02:49:12 -0300

    fix cors, add rota de imagens

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const routes = require('./routes');
+const cors = require('cors');
+const path = require('path');
+
 const mongoose = require('mongoose');
 
 const { username, password, database } = require('./credentials/mongodb-atlas.json');
@@ -11,7 +14,9 @@ mongoose.connect(`mongodb+srv://${username}:${password}@cluster0-3nmv8.mongodb.n
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 console.log('server on');
