4dad4599ce91446f5e169a97d57d714b6ea62e16
Hebert Lima <hebert.dev@hotmail.com>
2019-10-05 04:48:02 -0300

    add rotas para aprovar e rejeitar reservas

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -9,9 +9,13 @@ const AuthController = require('./controllers/AuthController');
 
 const SpotController = require('./controllers/SpotController');
 
+const BookingController = require('./controllers/BookingController');
+
 const DashboardController = require('./controllers/DashboardController');
 
-const BookingController = require('./controllers/BookingController');
+const ApprovalController = require('./controllers/ApprovalController');
+
+const RejectionController = require('./controllers/RejectionController');
 
 routes.post('/auth', AuthController.store);
 
@@ -20,6 +24,10 @@ routes.get('/spots', SpotController.index);
 routes.post('/spots', upload.single('thumbnail'), SpotController.store);
 
 routes.post('/spots/:id/bookings', BookingController.store);
+
+routes.post('/bookings/:id/rejections', RejectionController.store);
+
+routes.post('/bookings/:id/approvals', ApprovalController.store);
 
 routes.get('/dashboard', DashboardController.show);
 
